d4c4f20b4e2c3285227c0ed45982a4f28cad3397
t3t30 <mateusbie6@gmail.com>
2025-01-03 03:24:40 -0300

    binding all and add constraints

--- app/src/main/java/com/example/investeasy/MainActivity.kt
+++ app/src/main/java/com/example/investeasy/MainActivity.kt
@@ -5,12 +5,19 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.investeasy.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         }
+
+
+
+
+
     }
